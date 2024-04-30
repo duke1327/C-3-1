ebb27d660e51f39861ae8d79ae5cf62d9e1a0d07
duke1327 <dr9766@naver.com>
2024-04-30 11:39:53 +0900

    9주차 실습 내용

---
+++ 9th/sln_9/project_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_5
+{
+    internal class Program
+    {
+        class Product
+        {
+            public string name;
+            public int price;
+        }
+
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            
+            p.name = "감자";
+            p.price = 5000;
+            Console.WriteLine(p.name + " : " + p.price + "원");
+            
+
+            Product pA = new Product() { name = "감자", price = 5000 };
+            Product pB = new Product() { name = "고구마", price = 6000 };
+            Product pC = new Product() { name = "호박", price = 4000 };
+
+            Console.WriteLine($"{pA.name} : {pA.price}원");
+            Console.WriteLine($"{pB.name} : {pB.price}원");
+            Console.WriteLine($"{pC.name} : {pC.price}원");
+        }
+    }
+}
