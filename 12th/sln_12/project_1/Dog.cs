ab949cf9c61841acf5935fe505ec645991a74b60
duke1327 <dr9766@naver.com>
2024-05-21 11:45:11 +0900

    12주차 실습 내용

---
+++ 12th/sln_12/project_1/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_1
+{
+    internal class Dog
+    {
+        public int Age { get; set; } // 속성
+        public string Color { get; set; } // 속성
+        
+        public Dog() { this.Age = 0; } // 생성자
+
+        public void Eat() { Console.WriteLine("냠냠"); } // 메서드
+        public void Sleep() { Console.WriteLine("쿨쿨"); }
+        public void Bark() { Console.WriteLine("왈왈"); }
+    }
+}
