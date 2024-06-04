048ff06e5adeb20d119bfb61807b4abf86fced60
duke1327 <dr9766@naver.com>
2024-06-04 11:53:00 +0900

    14주차 실습 내용

--- 14th/sln_14/Delegate/Program.cs
+++ 14th/sln_14/Delegate/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Delegate
 {
+    delegate int MyDelegate(int a, int b); // 델리게이트(대리자) 선언
+
+    class Calculator
+    {
+        public int Plus(int a, int b) { return a + b; }
+        public static int Minus(int a, int b) { return a - b; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -14,8 +23,19 @@ namespace Delegate
             // C / C++ 언어에서의 함수 포인터와 유사한 기능
             // 함수 포인터보다 객체지향적이며, 타입 안정적이다.
             // 대리자를 사용하여 콜백을 구현하는 과정
-            // ① 대리자를 선언한다.
-            // ② 대리자의 인스턴스를 생성한다.
+            // ⑴ 대리자를 선언한다.
+            // ⑵ 대리자의 인스턴스를 생성한다.
+            //    인스턴스를 생성할 때는 대리자가 참조할
+            //    메서드를 매개변수로 넘긴다.
+            // ⑶ 대리자를 호출한다.
+
+            Calculator calc = new Calculator();
+            MyDelegate Calback;
+            Calback= new MyDelegate(calc.Plus);
+            Console.WriteLine(Calback(3, 4));
+            //Calback = new MyDelegate(calc.Minus);
+            Calback = new MyDelegate(Calculator.Minus);
+            Console.WriteLine(Calback(7, 4));
         }
     }
 }
---
+++ 14th/sln_14/SortWithDelegate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortWithDelegate
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+    internal class Program
+    {
+        // 메서드는 프로그램 안으로 넣을 것
+        static int SortWithPrice(Product a, Product b) // Delegate로 사용 : 미리 만들어서 제공하는 키워드
+        {
+            return a.Price.CompareTo(b.Price); // CompareTo : 비교
+        }
+
+        static int SortWithName(Product a, Product b)
+        {
+            return a.Name.CompareTo(b.Name);
+        }
+
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() {Name="감자",Price=3000},
+                new Product() {Name="고구마",Price=4000},
+                new Product() {Name="호박",Price=2000},
+                new Product() {Name="당근",Price=3500},
+                new Product() {Name="연근",Price=2500}
+            };
+
+            // 가격 정렬
+            products.Sort(SortWithPrice);
+
+            // 출력
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine();
+
+            // 이름 정렬
+            products.Sort(SortWithName);
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+        }
+    }
+}
