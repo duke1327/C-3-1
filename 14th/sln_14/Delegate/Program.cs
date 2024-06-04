f9af82e2dd015c1cbaf198636f9bd6a0db51253a
duke1327 <dr9766@naver.com>
2024-06-04 09:28:25 +0900

    주석 처리 확인용 푸시

---
+++ 14th/sln_14/Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 델리게이트(대리자) : 대리자는 메서드에 대한 참조이다.
+            // C / C++ 언어에서의 함수 포인터와 유사한 기능
+            // 함수 포인터보다 객체지향적이며, 타입 안정적이다.
+            // 대리자를 사용하여 콜백을 구현하는 과정
+            // ① 대리자를 선언한다.
+            // ② 대리자의 인스턴스를 생성한다.
+        }
+    }
+}
